1c0179643a303cc74ace11f9a1ea1bd13c5608aa
Taek0e <bnskim05@gmail.com>
2022-01-03 22:11:04 +0900

    2022.01.03 C# 공부 했음

---
+++ CSpractice/CSpractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("정수를 입력하시오.");
+            string inputNum = Console.ReadLine();
+            int num = int.Parse(inputNum);
+
+            Console.WriteLine($"당신이 입력한 수는 {num}입니다.");
+        }
+    }
+}

198664f5eccd8bfdd6de3754a9cd3f8312f8aabc
Taek0e <bnskim05@gmail.com>
2022-01-04 22:59:46 +0900

    2022.01.04 유니티 물리 코드 코드 구현 잘했어 태경아 할수잇다. 화이팅 !

--- CSpractice/CSpractice/Program.cs
+++ CSpractice/CSpractice/Program.cs
@@ -4,25 +4,24 @@ namespace CSpractice
 {
     class Program
     {
-        //int fireangle = 0;
-        //while (true)
-        //{
-        //    gameobject tempobject = instantiate(bulletobject, bulletcontainer, true);
-        // bulletcontainer안에 bulletobject를 생성, tempobject변수에 생성된 총알옵젝을 저장.
-        //vector2 direction = new vector2(mathf.cos(fireangle * mathf.deg2rad), mathf.sin(fireangle * mathf.deg2rad));
-        // 벡터2 direction 변수에 fireangle값 만큼 코사인, 사인으로 각도(위치) 지정. (math.cos,sin은 radian값을 받음으로 mathf.deg2rad를 곱해주어 degree를 radian으로 변환
-        //tempobject.transform.right = direction;
-        //     tempobject에 있는 총알옵젝의 오른쪽(x축의 양)을 (0도)기준으로 direction 각 만큼 회전값 설정
-        //    tempobject.transform.position = transform.position;
-        //     총알옵젝의 위치를 플레이어 위치로 설정 
+    //    while (true)
+    //    {
+    //        for (int fireAngle = startAngle; fireAngle<endAngle; fireAngle += angleInterval)
+    //        {
+    //          1.fireAngle = 30도(첫 총알 각도)
+    //          2.fireAngle 값이 330도 보다 작다면 실행.
+    //          3.다음 총알의 각도르 10도 up.
 
-        //    yield return new waitforseconds(0.1f);
-        // 0.1f초 기다리고
+    //            GameObject tempObject = Instantiate(bulletObject, bulletContainer, true);
+    //            Vector2 direction = new Vector2(Mathf.Cos(fireAngle * Mathf.Deg2Rad), Mathf.Sin(fireAngle * Mathf.Deg2Rad));
 
-        //fireangle += angleinterval;
-        //    총알의 회전값을 angleinterval 값 만큼 추가 
-        //    if (fireangle > 360) fireangle = 0;
-        //     각도가 360을 초과하면 1도 부터
-        }
-}
+    //            tempObject.transform.right = direction;
+                
+    //            tempObject.transform.position = transform.position;
+    //        }
+
+    //        yield return new WaitForSeconds(4f);
+    //          4초 대기
+    //}
+    }
 }

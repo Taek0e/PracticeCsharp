a6cc9fbbc0a6c7698b33dabb328c2720ae758094
Taek0e <bnskim05@gmail.com>
2022-01-04 22:56:41 +0900

    2022.01.04 야무지게 유니티 물리 공부 했다. 화이팅 김태경.

--- CSpractice/CSpractice/Program.cs
+++ CSpractice/CSpractice/Program.cs
@@ -4,13 +4,25 @@ namespace CSpractice
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.Write("정수를 입력하시오.");
-            string inputNum = Console.ReadLine();
-            int num = int.Parse(inputNum);
+        //int fireangle = 0;
+        //while (true)
+        //{
+        //    gameobject tempobject = instantiate(bulletobject, bulletcontainer, true);
+        // bulletcontainer안에 bulletobject를 생성, tempobject변수에 생성된 총알옵젝을 저장.
+        //vector2 direction = new vector2(mathf.cos(fireangle * mathf.deg2rad), mathf.sin(fireangle * mathf.deg2rad));
+        // 벡터2 direction 변수에 fireangle값 만큼 코사인, 사인으로 각도(위치) 지정. (math.cos,sin은 radian값을 받음으로 mathf.deg2rad를 곱해주어 degree를 radian으로 변환
+        //tempobject.transform.right = direction;
+        //     tempobject에 있는 총알옵젝의 오른쪽(x축의 양)을 (0도)기준으로 direction 각 만큼 회전값 설정
+        //    tempobject.transform.position = transform.position;
+        //     총알옵젝의 위치를 플레이어 위치로 설정 
 
-            Console.WriteLine($"당신이 입력한 수는 {num}입니다.");
+        //    yield return new waitforseconds(0.1f);
+        // 0.1f초 기다리고
+
+        //fireangle += angleinterval;
+        //    총알의 회전값을 angleinterval 값 만큼 추가 
+        //    if (fireangle > 360) fireangle = 0;
+        //     각도가 360을 초과하면 1도 부터
         }
-    }
+}
 }

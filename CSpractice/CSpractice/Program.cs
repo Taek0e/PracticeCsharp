0d8d40febc5cea3098ea1ef1190a359902c13a2d
Taek0e <bnskim05@gmail.com>
2022-01-06 23:13:10 +0900

    2022.01.06 태경 괜찮 잘하고 있음..!

--- CSpractice/CSpractice/Program.cs
+++ CSpractice/CSpractice/Program.cs
@@ -1,27 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace CSpractice
 {
     class Program
     {
-    //    while (true)
-    //    {
-    //        for (int fireAngle = startAngle; fireAngle<endAngle; fireAngle += angleInterval)
-    //        {
-    //          1.fireAngle = 30도(첫 총알 각도)
-    //          2.fireAngle 값이 330도 보다 작다면 실행.
-    //          3.다음 총알의 각도르 10도 up.
+        
 
-    //            GameObject tempObject = Instantiate(bulletObject, bulletContainer, true);
-    //            Vector2 direction = new Vector2(Mathf.Cos(fireAngle * Mathf.Deg2Rad), Mathf.Sin(fireAngle * Mathf.Deg2Rad));
+        //if (Input.GetMouseButtonDown(0))  
+        //{
+        //    Vector2 mousePos = Input.mousePosition;
+        //mousePos = mainCamera.ScreenToWorldPoint(mousePos);
 
-    //            tempObject.transform.right = direction;
-                
-    //            tempObject.transform.position = transform.position;
-    //        }
+        //    Vector3 playerPos = transform.position;
 
-    //        yield return new WaitForSeconds(4f);
-    //          4초 대기
-    //}
-    }
+        //Vector2 dirVec = mousePos - (Vector2)playerPos;
+        //dirVec = dirVec.normalized;
+
+        //    GameObject tempObject = Instantiate(bulletObject, bulletContainer);
+        //tempObject.transform.right = dirVec;
+
+        //    tempObject.transform.position = (Vector2) playerPos + dirVec* 0.5f;
+            
+        //    transform.Translate(-dirVec);
+        //}
+
+
+}
 }

8dc0fc4a872489e0229f4d20fbfb02b1011c0a79
Taek0e <bnskim05@gmail.com>
2022-01-08 14:42:34 +0900

    2022.01.08 벡터의 내적을 황용한 유니티 게임 기능

--- CSpractice/CSpractice/Program.cs
+++ CSpractice/CSpractice/Program.cs
@@ -6,23 +6,28 @@ namespace CSpractice
 {
     class Program
     {
-        // 마우스 왼쪽 클릭 실행 기능
-        //if (Input.GetMouseButtonDown(0))  
+        //void CheckGhost()
         //{
-        //    Vector2 mousePos = Input.mousePosition;
-        //mousePos = mainCamera.ScreenToWorldPoint(mousePos);
+        //    int i = 0;  // 초기 값은 0
+        //    foreach (var ghost in ghostObjectArray)  // foreach문을 이용해서 배열에 있는 유령옵젝들을 전부 체크.
+        //    {
+        //        Vector3 distanceVec = ghost.transform.position - transform.position; // 벡터3 distanceVec에는 내 위치에서 유령옵젝으로 향하는 벡터를 할당시킴.
 
-        //    Vector3 playerPos = transform.position;
+        //        if (distanceVec.magnitude < rangeDistance)  // rangeDistance의 크기 보다 distanceVec의 크기가 작다면 실행.(벡터.magnitude는 벡터의 크기만 나타냄 즉, 스칼라로 바뀜)
+        //        {
+        //            Vector3 dirVec = distanceVec.normalized;  // 내적을 하기 위해 distanceVec를 방향벡터로 바꾸어줌 (벡터.normalized를 하면 벡터의 방향만 나타냄 즉, 방향벡터로 바뀜)
 
-        //Vector2 dirVec = mousePos - (Vector2)playerPos;
-        //dirVec = dirVec.normalized;
+        //            if (Vector3.Dot(transform.up, dirVec) > Mathf.Cos(rangeAngle * Mathf.Deg2Rad)) // transform.up은 옵젝의 양의 y축 방향이다. (transform.up,transform.right,.. Vector.up,down,right 등.. 모두 크기가 1인 방향벡터이다.)
+        //                i++;
 
-        //    GameObject tempObject = Instantiate(bulletObject, bulletContainer);
-        //tempObject.transform.right = dirVec;
 
-        //    tempObject.transform.position = (Vector2) playerPos + dirVec* 0.5f;
-            
-        //    transform.Translate(-dirVec);
+        //            // 벡터.Dot(벡터1, 벡터2) 를 하면, 벡터1 벡터2의 내적한 값이 나온다. 
+
+        //            // 두 벡터를 내적한 값 > cos세타 이므로, 두 벡터를 내적한 값이 더 크다면 감지된것으로 판단하고, i++을 해준다. 
+        //        }
+        //    }
+
+        //    Debug.Log("감지된 유령의 수: " + i);
         //}
-}
+    }
 }

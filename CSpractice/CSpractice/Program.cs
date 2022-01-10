f0c974f28d85d3091488a126d7251c7020102448
Taek0e <bnskim05@gmail.com>
2022-01-10 17:18:56 +0900

    2022.01.10 - 외적을 활용한 마리오 블록이벤트 구현코드

--- CSpractice/CSpractice/Program.cs
+++ CSpractice/CSpractice/Program.cs
@@ -6,28 +6,30 @@ namespace CSpractice
 {
     class Program
     {
-        //void CheckGhost()
+        //        유니티 꿀팁상식 : Vector3.right와 transform.right는 다르다.
+        //Vector3는 월드를 기준으로 절대적인 오른쪽을 가르키고,
+        //transform은 오브젝트를 기준으로 오른쪽을 가르킨다.
+
+
+        //distVec = 내위치
+
+        //_col 옵젝의 오른쪽 방향벡터와 distVec벡터를 외적한 z축의 값이 양수일때 실행.
+
+
+        //- 벡터의 외적 오른손의 법칙
+        //반시계 방향 - 엄지의 방향(화면에서 밖으로) - z값 : 양수 : 벽의 위에 부딪힘
+        //시계 방향 - 엄지의 방향(밖에서 화면으로) - z값 : 음수 : 벽의 아래에 부딪힘
+
+        //void UpOrDown(Collision2D _col)
         //{
-        //    int i = 0;  // 초기 값은 0
-        //    foreach (var ghost in ghostObjectArray)  // foreach문을 이용해서 배열에 있는 유령옵젝들을 전부 체크.
+        //    Vector3 distVec = transform.position - _col.transform.position;
+
+        //    if (Vector3.Cross(_col.transform.right, distVec).z > 0)
         //    {
-        //        Vector3 distanceVec = ghost.transform.position - transform.position; // 벡터3 distanceVec에는 내 위치에서 유령옵젝으로 향하는 벡터를 할당시킴.
-
-        //        if (distanceVec.magnitude < rangeDistance)  // rangeDistance의 크기 보다 distanceVec의 크기가 작다면 실행.(벡터.magnitude는 벡터의 크기만 나타냄 즉, 스칼라로 바뀜)
-        //        {
-        //            Vector3 dirVec = distanceVec.normalized;  // 내적을 하기 위해 distanceVec를 방향벡터로 바꾸어줌 (벡터.normalized를 하면 벡터의 방향만 나타냄 즉, 방향벡터로 바뀜)
-
-        //            if (Vector3.Dot(transform.up, dirVec) > Mathf.Cos(rangeAngle * Mathf.Deg2Rad)) // transform.up은 옵젝의 양의 y축 방향이다. (transform.up,transform.right,.. Vector.up,down,right 등.. 모두 크기가 1인 방향벡터이다.)
-        //                i++;
-
-
-        //            // 벡터.Dot(벡터1, 벡터2) 를 하면, 벡터1 벡터2의 내적한 값이 나온다. 
-
-        //            // 두 벡터를 내적한 값 > cos세타 이므로, 두 벡터를 내적한 값이 더 크다면 감지된것으로 판단하고, i++을 해준다. 
-        //        }
+        //        Debug.Log("Up");
+        //        return;
         //    }
-
-        //    Debug.Log("감지된 유령의 수: " + i);
+        //    Debug.Log("Down");
         //}
     }
 }

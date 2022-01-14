9ab2be77b85d90e278680dc9e128713d917e1edf
Taek0e <bnskim05@gmail.com>
2022-01-14 14:39:41 +0900

    2022.01.14 월드투매트릭스 행렬 사용 코드

--- CSpractice/CSpractice/Program.cs
+++ CSpractice/CSpractice/Program.cs
@@ -6,21 +6,15 @@ namespace CSpractice
 {
     class Program
     {
-        //Vector3 tran = new Vector3(2, 1, 0);
-        //Quaternion rot = Quaternion.Euler(45, 0, 45);
-        //Vector3 scal = new Vector3(3, 3, 3);
+        //Matrix4x4 matrix = transform.localToWorldMatrix;
+        //// localToWorldMatrix를 저장하겠다. 
+        //// localToWorldMatrix값 == TRS 값
 
-        //worldMat = Matrix4x4.TRS(tran, rot, scal);  
-        //// TRS연산 함수를 호출하여 편리하게 TRS를 연산하는 방식.
-
-        //worldMat = Matrix4x4.Translate(new Vector3(2, 1, 0)) * Matrix4x4.Rotate(rot)* Matrix4x4.Scale(new Vector3(3, 3, 3));
-        //// TRS연산 이동행렬, 회전행렬, 크기변환행렬을 하나씩 만들어서 순서대로 곱해주는 방식.
-
-
-        //Debug.Log("==== Make Matrix ====");
+        //Debug.Log("=== Extraction Matrix ===");
         //for (int i = 0; i< 4; i++)
         //{
-        //    Debug.Log(worldMat.GetRow(i));
+        //    Debug.Log(matrix.GetRow(i));
+        //    // Column(열) , Row(행)
         //}
 
 }

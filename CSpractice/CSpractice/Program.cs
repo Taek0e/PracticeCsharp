d528b88a607446fed1226f9aa6c6e7e2061c732c
Taek0e <bnskim05@gmail.com>
2022-01-14 14:37:11 +0900

    2022.01.14 유니티 C# TRS 행렬 사용 코드

--- CSpractice/CSpractice/Program.cs
+++ CSpractice/CSpractice/Program.cs
@@ -6,34 +6,22 @@ namespace CSpractice
 {
     class Program
     {
-        // 태경이는 오늘 커밋함 ㅋㅋ ㄹㅇ ㅋㅋ
+        //Vector3 tran = new Vector3(2, 1, 0);
+        //Quaternion rot = Quaternion.Euler(45, 0, 45);
+        //Vector3 scal = new Vector3(3, 3, 3);
 
-        //        유니티 꿀팁상식 : Vector3.right와 transform.right는 다르다.
-        //Vector3는 월드를 기준으로 절대적인 오른쪽을 가르키고,
-        //transform은 오브젝트를 기준으로 오른쪽을 가르킨다.
+        //worldMat = Matrix4x4.TRS(tran, rot, scal);  
+        //// TRS연산 함수를 호출하여 편리하게 TRS를 연산하는 방식.
+
+        //worldMat = Matrix4x4.Translate(new Vector3(2, 1, 0)) * Matrix4x4.Rotate(rot)* Matrix4x4.Scale(new Vector3(3, 3, 3));
+        //// TRS연산 이동행렬, 회전행렬, 크기변환행렬을 하나씩 만들어서 순서대로 곱해주는 방식.
 
 
-        //distVec = 내위치
-
-        //_col 옵젝의 오른쪽 방향벡터와 distVec벡터를 외적한 z축의 값이 양수일때 실행.
-
-
-        //- 벡터의 외적 오른손의 법칙
-        //반시계 방향 - 엄지의 방향(화면에서 밖으로) - z값 : 양수 : 벽의 위에 부딪힘
-        //시계 방향 - 엄지의 방향(밖에서 화면으로) - z값 : 음수 : 벽의 아래에 부딪힘
-
-        //void UpOrDown(Collision2D _col)
+        //Debug.Log("==== Make Matrix ====");
+        //for (int i = 0; i< 4; i++)
         //{
-        //    Vector3 distVec = transform.position - _col.transform.position;
-
-        //    if (Vector3.Cross(_col.transform.right, distVec).z > 0)
-        //    {
-        //        Debug.Log("Up");
-        //        return;
-        //    }
-        //    Debug.Log("Down");
+        //    Debug.Log(worldMat.GetRow(i));
         //}
 
-
-    }
+}
 }

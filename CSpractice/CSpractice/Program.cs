54662231a2ac770ca3baf51caa1bed91ec192f3d
Taek0e <bnskim05@gmail.com>
2022-01-28 11:53:27 +0900

    ,열심히 좀 하자 ㅠㅜ

--- CSpractice/CSpractice/Program.cs
+++ CSpractice/CSpractice/Program.cs
@@ -6,20 +6,10 @@ namespace CSpractice
 {
     class Program
     {
-        //Debug.Log(boxRigidbody.velocity);
-
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    boxRigidbody.AddForce(transform.right* movePower, ForceMode.Impulse);
-
-        //    boxRigidbody.AddForce(transform.right* movePower / Time.fixedDeltaTime, ForceMode.Force);
-        //}
-        //else if (Input.GetKey(KeyCode.S))
-        //    boxRigidbody.AddForce(transform.right* movePower, ForceMode.Force);
-        //else if (Input.GetKeyDown(KeyCode.D))
-        //    boxRigidbody.AddForce(transform.right* movePower, ForceMode.VelocityChange);
-        //else if (Input.GetKey(KeyCode.F))
-        //    boxRigidbody.AddForce(transform.right* movePower, ForceMode.Acceleration);
-
-}
+        void ClientProcess()
+        {
+           // ㅈㅅㅈㅅ
+                
+        }
+    }
 }

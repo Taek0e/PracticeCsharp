bbdd0f23d1fa7acee280a1b6679d67c5365c70e8
Taek0e <bnskim05@gmail.com>
2022-02-19 16:19:45 +0900

    2022.02.19 Stack and Queue Study

---
+++ PracticeCScode/PracticeCS - 복사본/PracticeCS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PracticeCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(101);
+            queue.Enqueue(102);
+
+            int data = queue.Dequeue();
+            Console.WriteLine(data);
+        }
+    }
+}

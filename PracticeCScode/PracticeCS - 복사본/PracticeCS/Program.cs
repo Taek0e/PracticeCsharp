f1d8aa9bc0d7b1d13c3d4d4184f9579807c2f238
Taek0e <bnskim05@gmail.com>
2022-02-23 17:50:34 +0900

    2022.02.23 DFS 알고리즘.

--- PracticeCScode/PracticeCS - 복사본/PracticeCS/Program.cs
+++ PracticeCScode/PracticeCS - 복사본/PracticeCS/Program.cs
@@ -11,8 +11,8 @@ namespace PracticeCS
             { 0, 1, 0, 1, 0, 0 },
             { 1, 0, 1, 1, 0, 0 },
             { 0, 1, 0, 0, 0, 0 },
-            { 1, 1, 0, 0, 1, 0 },
-            { 0, 0, 0, 1, 0, 1 },
+            { 1, 1, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 1 },
             { 0, 0, 0, 0, 1, 0 },
         };
 
@@ -21,18 +21,59 @@ namespace PracticeCS
             new List<int>() { 1, 3},
             new List<int>() { 0, 2, 3 },
             new List<int>() { 1 },
-            new List<int>() { 0, 1, 4 },
-            new List<int>() { 3, 5 },
+            new List<int>() { 0, 1},
+            new List<int>() { 5 },
             new List<int>() { 4 },
         };
-        
+
+        bool[] visited = new bool[6];
+        public void DFS(int now)
+        {
+            Console.WriteLine(now);
+            visited[now] = true;
+                
+            for (int next = 0; next < 6; next++)
+            {
+                if (adj[now, next] == 0)
+                    continue;
+                if (visited[next])
+                    continue;
+                DFS(next); 
+            }
+        }
+
+        public void DFS2(int now)
+        {
+            Console.WriteLine(now);
+            visited[now] = true;
+
+            foreach (int next in adj2[now])
+            {
+                if (visited[next])
+                    continue;
+                DFS2(next); 
+            }
+
+        }
+
+        public void SearchAll()
+        {
+            visited = new bool[6];
+            for (int now = 0; now < 6; now++)
+                if (visited[now] == false)
+                    DFS(now);   
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Graph graph = new Graph();
+       
+            graph.SearchAll();
         }
     }
+
+    
 }

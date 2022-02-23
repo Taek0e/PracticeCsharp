6ec02aa83cc122db74834d30caf14b5b8626e2d9
Taek0e <bnskim05@gmail.com>
2022-02-23 14:21:53 +0900

    2022.02.23 백준 설탕배달 2839

---
+++ AlgoCScode/AlgoCS - 복사본/AlgoCS/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AlgoCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            if (N >= 3 && N <= 5000)
+            {
+                while (N > 0)
+                {
+                    if (N % 5 == 0)
+                    {
+                        N -= 5;
+                        count++;
+                    }
+                    else if (N % 3 == 0)
+                    {
+                        N -= 3;
+                        count++;
+                    }
+                    else if (N >= 5)
+                    {
+                        N -= 5;
+                        count++;
+                    }
+                    else if (N >= 3)
+                    {
+                        N -= 3;
+                        count++;
+                    }
+                    else
+                    {
+                        count = -1;
+                        break;
+                    }
+                }
+
+                Console.WriteLine(count);
+            }
+            
+            
+
+
+
+        }
+    }
+}

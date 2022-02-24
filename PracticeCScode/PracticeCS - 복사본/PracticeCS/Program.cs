7cb2e0c2daf3d6bc5b348e5b36f3fbd064a2e285
Taek0e <bnskim05@gmail.com>
2022-02-24 23:36:30 +0900

    2022.02.24 BFS (너비 우선 탐색) 알고리즘

--- PracticeCScode/PracticeCS - 복사본/PracticeCS/Program.cs
+++ PracticeCScode/PracticeCS - 복사본/PracticeCS/Program.cs
@@ -11,8 +11,8 @@ namespace PracticeCS
             { 0, 1, 0, 1, 0, 0 },
             { 1, 0, 1, 1, 0, 0 },
             { 0, 1, 0, 0, 0, 0 },
-            { 1, 1, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 1 },
+            { 1, 1, 0, 0, 1, 0 },
+            { 0, 0, 0, 1, 0, 1 },
             { 0, 0, 0, 0, 1, 0 },
         };
 
@@ -21,48 +21,39 @@ namespace PracticeCS
             new List<int>() { 1, 3},
             new List<int>() { 0, 2, 3 },
             new List<int>() { 1 },
-            new List<int>() { 0, 1},
-            new List<int>() { 5 },
+            new List<int>() { 0, 1, 4},
+            new List<int>() { 3, 5 },
             new List<int>() { 4 },
         };
 
-        bool[] visited = new bool[6];
-        public void DFS(int now)
+
+
+        public void BFS(int start)
         {
-            Console.WriteLine(now);
-            visited[now] = true;
-                
-            for (int next = 0; next < 6; next++)
+            bool[] found = new bool[6];
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+
+            while(q.Count > 0)
             {
-                if (adj[now, next] == 0)
-                    continue;
-                if (visited[next])
-                    continue;
-                DFS(next); 
+                int now = q.Dequeue();
+                Console.WriteLine(now);
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0)
+                        continue;
+                    if (found[next])
+                        continue;
+
+                    q.Enqueue(next);
+                    found[next] = true;
+                }
             }
         }
-
-        public void DFS2(int now)
-        {
-            Console.WriteLine(now);
-            visited[now] = true;
-
-            foreach (int next in adj2[now])
-            {
-                if (visited[next])
-                    continue;
-                DFS2(next); 
-            }
-
-        }
-
-        public void SearchAll()
-        {
-            visited = new bool[6];
-            for (int now = 0; now < 6; now++)
-                if (visited[now] == false)
-                    DFS(now);   
-        }
+        
     }
 
     class Program
@@ -70,8 +61,8 @@ namespace PracticeCS
         static void Main(string[] args)
         {
             Graph graph = new Graph();
-       
-            graph.SearchAll();
+            graph.BFS(0);   
+           
         }
     }
 
